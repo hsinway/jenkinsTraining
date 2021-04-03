035f938739ef756c28b6b80ddfea3b5092a4d6eb
hsinway <xinwei01@live.com>
2021-04-03 16:02:50 +0800

    commit 4/3

--- jenkins_lesson/test_1.py
+++ jenkins_lesson/test_1.py
@@ -4,5 +4,9 @@ class TestDemo:
         print("hello jenkins!")
 
     def test_2(self):
-        a = "a"
-        print(f"i am back!{a}")
+        a = "again"
+        print(f"i am back! {a}")
+
+    def test_3(self):
+        l=[1,2,3,4,5]
+        print(l[-3:-1])
